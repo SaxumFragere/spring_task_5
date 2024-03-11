40349fa20c32fa92f848a0df0eb75a5d7ca397b1
Величко Анна Юрьевна <velichkoau@dev.vtb.ru>
2024-03-11 16:49:57 +0300

    first commit

--- src/main/java/org/example/Starter.java
+++ src/main/java/org/example/Starter.java
@@ -1,9 +1,7 @@
 package org.example;
 
-import org.example.repos.TppRefAccountTypeRepo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Starter {
